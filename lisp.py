e663646dc9e79729cfd9990ff7e206cbc076a696
Vítor De Araújo <vbuaraujo@users.noreply.github.com>
2024-01-19 20:06:44 +0000

    repl, running whole files, if, function definition part 1
    Co-authored-by: Ana Gilda Rodrigues <anagilda@users.noreply.github.com>
    Co-authored-by: pedro <pedro-vale@users.noreply.github.com>

--- lisp.py
+++ lisp.py
@@ -24,6 +24,7 @@
 
 program = """
 [define x 42]
+
 [print [+ x 1]]
 """
 
@@ -38,7 +39,8 @@ fac = """
 from typing import Any
 from dataclasses import dataclass
 import re
-
+import sys
+import traceback
 
 @dataclass
 class Symbol:
@@ -50,7 +52,13 @@ class Number:
     value: int
 
 
-Value = Symbol | Number | list['Value']
+@dataclass
+class Function:
+    parameters: list[str]
+    body: 'Value'
+
+
+Value = Symbol | Number | list['Value'] | Function
 
 
 def tokenize(text: str) -> list[str]:
@@ -76,6 +84,9 @@ def parse_expression(tokens: list[str]) -> tuple[Value, list[str]] | None:
             raise Exception("Expecting ], found end of program")
 
         return items, tokens[1:]
+
+    if tokens[0] == ']':
+        raise Exception("Unexpected ]")
 
     if tokens[0].isdigit():
         return Number(int(tokens[0])), tokens[1:]
@@ -110,11 +121,36 @@ def evaluate(expression: Value, environment: dict[str, Value]) -> Value:
             environment[name] = value
             return value
 
+        case [Symbol("if"), test, expression_if_true, expression_if_false]:
+            test_value = evaluate(test, environment)
+            if test_value:
+                return evaluate(expression_if_true, environment)
+            else:
+                return evaluate(expression_if_false, environment)
+
+        case [Symbol("fn"), parameters, body]:
+            if not isinstance(parameters, list):
+                raise Exception(f"Expected list of parameters, got {parameters} instead")
+
+            parameter_names = []
+            for parameter in parameters:
+                if not isinstance(parameter, Symbol):
+                    raise Exception(f"Expected parameter name, got {parameter} instead")
+                parameter_names.append(parameter.name)
+
+            function = Function(
+                parameters=parameter_names,
+                body=body,
+            )
+            return function
+
         case [head, *tail]:
             function = evaluate(head, environment)
             if callable(function):
                 arguments = [evaluate(expression, environment) for expression in tail]
                 return function(*arguments)
+            else:
+                raise Exception(f"{function} is not a function")
 
     raise Exception(f"Don't know how to evaluate {expression}")
 
@@ -125,6 +161,44 @@ standard_environment = {
     "-": lambda x, y: Number(x.value - y.value),
     "*": lambda x, y: Number(x.value * y.value),
     "/": lambda x, y: Number(x.value / y.value),
+    "<": lambda x, y: x.value < y.value,
+    ">": lambda x, y: x.value > y.value,
+    "print": print,
 }
 
 evaluate(parse_program(tokenize("[+ [* 3 4] [* 5 6]]"))[0], standard_environment)
+
+
+def evaluate_file(filename: str, environment: dict[str, Value]) -> None:
+    with open(filename) as program:
+        expressions = parse_program(tokenize(program.read()))
+        for expression in expressions:
+            evaluate(expression, environment)
+
+def repl(environment: dict[str, Value]) -> None:
+    while True:
+        try:
+            program = input("lisp> ")
+            expressions = parse_program(tokenize(program))
+            for expression in expressions:
+                result = evaluate(expression, environment)
+                print(result)
+
+        except EOFError:
+            return
+
+        except Exception as e:
+            traceback.print_exc()
+
+
+
+if __name__ == '__main__':
+    match sys.argv:
+        case [progname, filename]:
+            evaluate_file(filename, standard_environment)
+
+        case [progname]:
+            repl(standard_environment)
+
+        case [progname, *_]:
+            print(f"Usage: {progname} [filename]")
